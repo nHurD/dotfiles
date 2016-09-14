9b736744369d3a2837392a6139b03975d548a176
Jonathan Steele <jrsteele@gmail.com>
2016-09-14 14:44:22 -0400

    Update emacs / tmux
    Eliminate dependency on spacemacs in favor of using org mode to store my
    emacs configuration files.
    
    Also include my usual tmux configuration.

--- install.sh
+++ install.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
+# Setup emacs
+mkdir -p ~/.emacs.d
+ln -s emacs.d/init.el ~/.emacs.d/init.el
+ln -s emacs.d/jrsteele.org  ~/.emacs.d/jrsteele.org
+
+# Tmux
+ln -s tmux.conf ~/.tmux.conf
+
 # Install vim plugins
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -s "$(pwd)/vimrc" ~/.vimrc
 vim +PlugInstall +qall
 
-# # Spacemacs
-git clone --recursive http://github.com/syl20bnr/spacemacs ~/.emacs.d
-ln -s "$(pwd)/spacemacs" ~/.spacemacs
- 
 # Prezto
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 zsh -s <<EOF
@@ -18,6 +22,6 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 EOF
- 
+
 rm -rf ~/.zprezto/modules/prompt/functions/prompt_pure_setup
 ln -s "$(pwd)/prompt/prompt_pure_setup" ~/.zprezto/modules/prompt/functions/prompt_pure_setup
